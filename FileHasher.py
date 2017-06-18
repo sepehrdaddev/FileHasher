14e3d633319c87b141bf8df56dfcbff44af84151
sepehrdad <sepehrdad.dev@gmail.com>
2017-06-19 01:12:22 +0400

    Initial commit

--- FileHasher.py
+++ FileHasher.py
@@ -14,111 +14,119 @@ import argparse
 from colorama import *
 
 
-class FileHasher:
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def Getsize(infile):
+def Handler(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
         try:
-            units = {0: 'bytes', 1: 'Kbytes', \
-            2: 'Mbytes', 3: 'Gbytes', 4: 'Tbytes'}
-            size = os.path.getsize(infile)
-            i = 0
-            while size > 1000:
-                size = size / 1000
-                i += 1
-            return '%s %s' % (size, units[i])
-        except:
-            pass
+            func(*args, **kwargs)
+        except Exception as error:
+            print(Fore.RED + '\n[-] Error : %s' % error)
+        elapsed_time = time.time() - start_time
+        print(Fore.GREEN + '[+] Elapsed time: %s' % elapsed_time)
+    return wrapper
 
-    @staticmethod
-    def Base32(text):
-        encoded = base64.b32encode(text)
-        return encoded.decode('utf-8')
 
-    @staticmethod
-    def Base64(text):
-        encoded = base64.b64encode(text)
-        return encoded.decode('utf-8')
+def Getsize(infile):
+    units = {0: 'bytes', 1: 'Kbytes', \
+    2: 'Mbytes', 3: 'Gbytes', 4: 'Tbytes'}
+    size = os.path.getsize(infile)
+    i = 0
+    while size > 1000:
+        size = size / 1000
+        i += 1
+    return '%s %s' % (size, units[i])
 
-    @staticmethod
-    def Base85(text):
-        encoded = base64.b85encode(text)
-        return encoded.decode('utf-8')
 
-    @staticmethod
-    def handledirectory(path):
-        for (dirpath, _, filenames) in os.walk(path):
-            for filename in filenames:
-                yield os.path.join(dirpath, filename)
+def Base32(text):
+    encoded = base64.b32encode(text)
+    return encoded.decode('utf-8')
 
-    @staticmethod
-    def Checksum(infile, method, format):
-        try:
-            BUF_SIZE = 65536
-            hasher = None
-            if method == 'SHA1':
-                hasher = hashlib.sha1()
-            elif method == 'SHA256':
-                hasher = hashlib.sha256()
-            elif method == 'SHA512':
-                hasher = hashlib.sha512()
-            elif method == 'MD5':
-                hasher = hashlib.md5()
-            with open(infile, 'rb') as f:
-                while True:
-                    data = f.read(BUF_SIZE)
-                    if not data:
-                        break
-                    hasher.update(data)
-            if format == 'HEX':
-                return (os.path.realpath(infile), FileHasher.Getsize(infile), method,
-                        hasher.hexdigest(), '_' * 58)
-            elif format == 'Base32':
-                return (os.path.realpath(infile), FileHasher.Getsize(infile), method,
-                        FileHasher.Base32(hasher.digest()), '_' * 58)
-            elif format == 'Base64':
-                return (os.path.realpath(infile), FileHasher.Getsize(infile), method,
-                        FileHasher.Base64(hasher.digest()), '_' * 58)
-            elif format == 'Base85':
-                return (os.path.realpath(infile), FileHasher.Getsize(infile), method,
-                        FileHasher.Base85(hasher.digest()), '_' * 58)
-        except:
-            pass
 
-if __name__ == '__main__':
-    init(autoreset=True)
+def Base64(text):
+    encoded = base64.b64encode(text)
+    return encoded.decode('utf-8')
+
+
+def Base85(text):
+    encoded = base64.b85encode(text)
+    return encoded.decode('utf-8')
+
+
+def handledirectory(path):
+    for (dirpath, _, filenames) in os.walk(path):
+        for filename in filenames:
+            yield os.path.join(dirpath, filename)
+
+
+def Checksum(infile, method, type):
+    BUF_SIZE = 65536
+    hasher = None
+    hashed = None
+    if method == 'SHA1':
+        hasher = hashlib.sha1()
+    elif method == 'SHA256':
+        hasher = hashlib.sha256()
+    elif method == 'SHA512':
+        hasher = hashlib.sha512()
+    elif method == 'MD5':
+        hasher = hashlib.md5()
+    with open(infile, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            hasher.update(data)
+    if type == 'HEX':
+        hashed = hasher.hexdigest()
+    elif type == 'Base32':
+        hashed = Base32(hasher.digest())
+    elif type == 'Base64':
+        hashed = Base64(hasher.digest())
+    elif type == 'Base85':
+        hashed = Base85(hasher.digest())
+
+    return (os.path.realpath(infile), Getsize(infile), method,
+            hashed, '_' * 59)
+
+
+@Handler
+def main():
     files = 0
-    print(Fore.LIGHTYELLOW_EX + '[Info] FileHasher by sepehrdad sh')
+    print(Fore.LIGHTYELLOW_EX + '[Info] FileHasher by Sepehrdad Sh')
     parser = argparse.ArgumentParser()
     parser.add_argument('method', type=str, choices=['SHA1', 'SHA256', 'SHA512', 'MD5'], help='Hashing method to use')
     parser.add_argument('format', type=str, choices=['HEX', 'Base32', 'Base64', 'Base85'], help='Output format')
     parser.add_argument('files', nargs='+', help='File/s to hash')
     args = parser.parse_args()
-    start_time = time.time()
     for i in args.files:
-        if os.path.isfile(i):
-            try:
-                print(Fore.GREEN + '\n   %s %s %s' % ('_' * 20, '[New File Hashed]', '_' * 20) + Fore.YELLOW
-                      + '\n\n   File Name: %s\n   File Size: %s\n   %s: %s\n   %s'
-                      % (FileHasher.Checksum(i, args.method, args.format)))
-                files += 1
-            except:
-                continue
-        elif os.path.isdir(i):
-            filelist = FileHasher.handledirectory(i)
-            try:
-                for x in filelist:
-                    print(Fore.GREEN + '\n   %s %s %s' % ('_' * 20, '[New File Hashed]', '_' * 20) + Fore.YELLOW
-                          + '\n\n   File Name: %s\n   File Size: %s\n   %s: %s\n   %s'
-                          % (FileHasher.Checksum(x, args.method, args.format)))
-                    files += 1
-            except:
-                continue
+        if os.path.exists(i):
+            if os.access(i, os.R_OK):
+                if os.path.isfile(i):
+                    try:
+                        print(Fore.GREEN + '\n   %s %s %s' % ('_' * 20, '[New File Hashed]', '_' * 20) + Fore.YELLOW
+                              + '\n\n   File Name: %s\n   File Size: %s\n   %s: %s\n   %s'
+                              % (Checksum(i, args.method, args.format)))
+                        files += 1
+                    except:
+                        continue
+                elif os.path.isdir(i):
+                    filelist = handledirectory(i)
+                    try:
+                        for x in filelist:
+                            if os.access(x, os.R_OK):
+                                print(Fore.GREEN + '\n   %s %s %s' % ('_' * 20, '[New File Hashed]', '_' * 20)
+                                      + Fore.YELLOW + '\n\n   File Name: %s\n   File Size: %s\n   %s: %s\n   %s'
+                                      % (Checksum(x, args.method, args.format)))
+                                files += 1
+                            else:
+                                print(Fore.RED + '[-] Unable to access: %s' % os.path.realpath(x))
+                    except:
+                        continue
+            else:
+                print(Fore.RED + '[-] Unable to access: %s' % os.path.realpath(i))
         else:
-            pass
-    elapsed_time = time.time() - start_time
+            print(Fore.RED + '[-] Unable to find: %s' % i)
     print(Fore.GREEN + '\n[+] Files: %s' % files)
-    print(Fore.GREEN + '[+] Elapsed time: %s' % elapsed_time)
+if __name__ == '__main__':
+    init(autoreset=True)
+    main()
